827d6bcb73218bb8f3e3887c785ab09b74690777
Marc Mouries <Marc@iMac.fios-router.home>
2020-05-17 19:39:39 -0400

    clean up css

--- NetworkTree.js
+++ NetworkTree.js
@@ -6,9 +6,8 @@
   var defaultWidth = 400,
     defaultHeight = 250;
 
-  const margin = {     top: 10,     right: 50,    bottom: 10,     left: 50  }; 
+  const margin = { top: 10, right: 50, bottom: 10, left: 50 };
   var display = function (containerId, tree_data, options) {
-
     var element = document.getElementById(containerId);
     if (!element) {
       alert(
@@ -19,111 +18,116 @@
       return;
     }
 
-
-
     /***********
      *
      *   ORIENTATION : TOP
      *
      */
 
+    // Dimensions: 400 x 400
+    // used for the initial rendering
+    // width to height proportion
+    // its preserved as the chart is resized
+    var width = 400 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
 
+    var svg = d3
+      .select("#chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .call(responsivefy); // Call responsivefy to make the chart responsive
+    var g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+    // Convert data for a tree layout
+    var root = d3.hierarchy(tree_data);
+    var tree = d3.tree().size([height, width]);
+    tree(root);
 
-		
-		// Dimensions: 400 x 400 
-		// used for the initial rendering 
-		// width to height proportion 
-		// its preserved as the chart is resized 
-		var width = 400 - margin.left - margin.right; 
-		var height = 400 - margin.top - margin.bottom; 
+    // Create links
+    var link = g
+      .selectAll(".link")
+      .data(root.descendants().slice(1))
+      .enter()
+      .append("path")
+      .attr("class", "link")
+      .attr("d", function (d) {
+        return (
+          "M" +
+          d.y +
+          "," +
+          d.x +
+          "C" +
+          (d.y + d.parent.y) / 2 +
+          "," +
+          d.x +
+          " " +
+          (d.y + d.parent.y) / 2 +
+          "," +
+          d.parent.x +
+          " " +
+          d.parent.y +
+          "," +
+          d.parent.x
+        );
+      });
 
-    
-		var svg = d3.select('#chart'). 
-      append('svg'). 
-      attr('width', width + margin.left + margin.right). 
-      attr('height', height + margin.top + margin.bottom). 
-      call(responsivefy); // Call responsivefy to make the chart responsive 
-  var    g = svg.append("g").attr('transform', `translate(${margin.left}, ${margin.top})`); 
+    // Create nodes
+    var node = g
+      .selectAll(".node")
+      .data(root.descendants())
+      .enter()
+      .append("g")
+      .attr("class", function (d) {
+        return "node" + (d.children ? " node--internal" : " node--leaf");
+      })
+      .attr("transform", function (d) {
+        return "translate(" + d.y + "," + d.x + ")";
+      });
 
+    node.append("circle").attr("r", 2.5);
 
-// Convert data for a tree layout
-var root = d3.hierarchy(tree_data)
-var tree = d3.tree().size([height, width]);
-tree(root);
+    node
+      .append("text")
+      .attr("dy", 3)
+      .attr("x", function (d) {
+        return d.children ? -10 : 10;
+      })
+      .style("text-anchor", function (d) {
+        return d.children ? "end" : "start";
+      })
+      .text(function (d) {
+        return d.data.name;
+      });
 
+    function responsivefy(svg) {
+      // Container is the DOM element, svg is appended.
+      // Then we measure the container and find its
+      // aspect ratio.
+      const container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width"), 10),
+        height = parseInt(svg.style("height"), 10),
+        aspect = width / height;
 
+      // Add viewBox attribute to set the value to initial size
+      // add preserveAspectRatio attribute to specify how to scale
+      // and call resize so that svg resizes on page load
+      svg
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .attr("preserveAspectRatio", "xMinYMid")
+        .call(resize);
 
-// Create links
-var link = g.selectAll(".link")
-  .data(root.descendants().slice(1))
-  .enter().append("path")
-  .attr("class", "link")
-  .attr("d", function(d) {
-    return "M" + d.y + "," + d.x + "C" + (d.y + d.parent.y) / 2 + "," + d.x + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x + " " + d.parent.y + "," + d.parent.x;
-  });
+      d3.select(window).on("resize." + container.attr("id"), resize);
 
-
-  // Create nodes
-var node = g.selectAll(".node")
-.data(root.descendants())
-.enter()
-.append("g")
-.attr("class", function(d) {
-  return "node" + (d.children ? " node--internal" : " node--leaf");
-})
-.attr("transform", function(d) {
-  return "translate(" + d.y + "," + d.x + ")";
-})
-
-node.append("circle")
-.attr("r", 2.5);
-
-node.append("text")
-.attr("dy", 3)
-.attr("x", function(d) {
-  return d.children ? -10 : 10;
-})
-.style("text-anchor", function(d) {
-  return d.children ? "end" : "start";
-})
-.text(function(d) {
-  return d.data.name
-});
-		
-
-		
-
-
-		function responsivefy(svg) { 
-			
-			// Container is the DOM element, svg is appended. 
-			// Then we measure the container and find its 
-			// aspect ratio. 
-			const container = d3.select(svg.node().parentNode), 
-				width = parseInt(svg.style('width'), 10), 
-				height = parseInt(svg.style('height'), 10), 
-				aspect = width / height; 
-				
-			// Add viewBox attribute to set the value to initial size 
-			// add preserveAspectRatio attribute to specify how to scale 
-			// and call resize so that svg resizes on page load 
-			svg.attr('viewBox', `0 0 ${width} ${height}`). 
-			attr('preserveAspectRatio', 'xMinYMid'). 
-			call(resize); 
-			
-			d3.select(window).on('resize.' + container.attr('id'), resize); 
-
-			function resize() { 
-				const targetWidth = parseInt(container.style('width')); 
-				svg.attr('width', targetWidth); 
-				svg.attr('height', Math.round(targetWidth / aspect)); 
-			} 
-		} 
-
-
-
-    };
+      function resize() {
+        const targetWidth = parseInt(container.style("width"));
+        svg.attr("width", targetWidth);
+        svg.attr("height", Math.round(targetWidth / aspect));
+      }
+    }
+  };
 
   // Exports
   window.NetworkTree = {
